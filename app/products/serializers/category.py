9d18b3de4b7ec366df6516b8004c771db519ca1e
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-21 21:48:07 +0600

    init

--- app/products/serializers/category.py
+++ app/products/serializers/category.py
@@ -7,3 +7,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['children'] = CategorySerializer(instance.children.all(), many=True).data
+        return representation
